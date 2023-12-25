fe99d249aa3e8c2b807bb960cfdf1da9622bec58
Negusu12 <negusu00@yahoo.com>
2023-12-25 05:35:36 -0300

    prevent vist from the same ip address to be count

--- index.php
+++ index.php
@@ -4,18 +4,20 @@ include('connect.php');
 // Get promoter_id from the URL
 $promoter_id = isset($_GET['promoter_id']) ? intval($_GET['promoter_id']) : 0;
 
-// Check if promoter_id exists in referral_count table
-$checkQuery = "SELECT * FROM referral_count WHERE promoter_id = $promoter_id";
-$result = $con->query($checkQuery);
+// Get the visitor's IP address
+$ip_address = $_SERVER['REMOTE_ADDR'];
 
-if ($result->num_rows == 0) {
-    // If promoter_id doesn't exist, insert a new record
-    $insertQuery = "INSERT INTO referral_count (promoter_id, visit_count) VALUES ($promoter_id, 1)";
+// Check if the IP address has already visited for the given promoter_id
+$checkIpQuery = "SELECT * FROM referral_count WHERE promoter_id = $promoter_id AND ip_address = '$ip_address'";
+$resultIp = $con->query($checkIpQuery);
+
+if ($resultIp->num_rows == 0) {
+    // If the IP address is new, insert a new record and increment the visit count
+    $insertQuery = "INSERT INTO referral_count (promoter_id, visit_count, ip_address) VALUES ($promoter_id, 1, '$ip_address')";
     $con->query($insertQuery);
 } else {
-    // If promoter_id exists, update the visit count
-    $updateQuery = "UPDATE referral_count SET visit_count = visit_count + 1 WHERE promoter_id = $promoter_id";
-    $con->query($updateQuery);
+    // If the IP address has already visited, do not increment the visit count
+    // You may choose to do nothing here or log this action for reference
 }
 
 // Redirect to the specified website
